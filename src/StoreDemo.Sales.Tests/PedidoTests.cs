504e97f6cfb732e91b529f90dea2123f706f9c0f
Paulo Junior <ext.paulo.junior@zamp.com.br>
2023-01-02 14:48:35 -0300

    adicionando testes

--- src/StoreDemo.Sales.Tests/PedidoTests.cs
+++ src/StoreDemo.Sales.Tests/PedidoTests.cs
@@ -5,52 +5,67 @@ namespace StoreDemo.Sales.Tests
 {
     public class PedidoTests
     {
-        //[Fact(DisplayName = "Novo Item Pedido")]
-        //[Trait("Categoria", "Vendas - Pedido")]
-        //public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
-        //{
-        //    // Arrange
-        //    var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
-        //    var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
+        [Fact(DisplayName = "Novo Item Pedido")]
+        [Trait("Categoria", "Vendas - Pedido")]
+        public void AdicionarItemPedido_NovoPedido_DeveAtualizarValor()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var pedidoItem = new PedidoItem(Guid.NewGuid(), "Produto Teste", 2, 100);
 
-        //    // Act
-        //    pedido.AdicionarItem(pedidoItem);
+            // Act
+            pedido.AdicionarItem(pedidoItem);
 
-        //    // Assert
-        //    Assert.Equal(200, pedido.ValorTotal);
-        //}
+            // Assert
+            Assert.Equal(200, pedido.ValorTotal);
+        }
 
-        //[Fact(DisplayName = "Adicionar Item Pedido Existente")]
-        //[Trait("Categoria", "Vendas - Pedido")]
-        //public void AdicionarItemPedido_ItemExistente_DeveIncrementarUnidadesSomarValores()
-        //{
-        //    // Arrange
-        //    var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
-        //    var produtoId = Guid.NewGuid();
-        //    var pedidoItem = new PedidoItem(produtoId, "Produto Teste", 2, 100);
-        //    pedido.AdicionarItem(pedidoItem);
+        [Fact(DisplayName = "Adicionar Item Pedido Existente")]
+        [Trait("Categoria", "Vendas - Pedido")]
+        public void AdicionarItemPedido_ItemExistente_DeveIncrementarUnidadesSomarValores()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var produtoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(produtoId, "Produto Teste", 2, 100);
+            pedido.AdicionarItem(pedidoItem);
 
-        //    // Act
-        //    var pedidoItem2 = new PedidoItem(produtoId, "Produto Teste", 1, 100);
-        //    pedido.AdicionarItem(pedidoItem2);
+            // Act
+            var pedidoItem2 = new PedidoItem(produtoId, "Produto Teste", 1, 100);
+            pedido.AdicionarItem(pedidoItem2);
 
-        //    // Assert
-        //    Assert.Equal(300, pedido.ValorTotal);
-        //    Assert.Equal(1, pedido.PedidoItens.Count);
-        //    Assert.Equal(3, pedido.PedidoItens.FirstOrDefault(p => p.ProdutoId == produtoId).Quantidade);
-        //}
+            // Assert
+            Assert.Equal(300, pedido.ValorTotal);
+            Assert.Equal(1, pedido.PedidoItens.Count);
+            Assert.Equal(3, pedido.PedidoItens.FirstOrDefault(p => p.ProdutoId == produtoId).Quantidade);
+        }
 
-        //[Fact(DisplayName = "Novo Item Pedido com Unidades Acima do Permitido")]
-        //[Trait("Categoria", "Vendas - Pedido")]
-        //public void AdicionarItemPedido_UnidadesItemAcimaDoPermitido_DeveRetornarException()
-        //{
-        //    // Arrange
-        //    var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
-        //    var produtoId = Guid.NewGuid();
-        //    var pedidoItem = new PedidoItem(produtoId, "Produto Teste", Pedido.MAX_UNIDADES_ITEM + 1, 100);
+        [Fact(DisplayName = "Novo Item Pedido com Unidades Acima do Permitido")]
+        [Trait("Categoria", "Vendas - Pedido")]
+        public void AdicionarItemPedido_UnidadesItemAcimaDoPermitido_DeveRetornarException()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var produtoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(produtoId, "Produto Teste", Pedido.MAX_UNIDADES_ITEM + 1, 100);
 
-        //    // Act & Assert
-        //    Assert.Throws<DomainException>(() => pedido.AdicionarItem(pedidoItem));
-        //}
+            // Act & Assert
+            Assert.Throws<DomainException>(() => pedido.AdicionarItem(pedidoItem));
+        }
+
+        [Fact(DisplayName = "Item Pedido Existente com Unidades Acima do Permitido")]
+        [Trait("Categoria", "Vendas - Pedido")]
+        public void AdicionarItemPedido_ItemExistenteSomaUnidadesAcimaDoPermitido_DeveRetornarException()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var produtoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(produtoId, "Produto Teste", 1, 100);
+            var pedidoItem2 = new PedidoItem(produtoId, "Produto Teste", Pedido.MAX_UNIDADES_ITEM, 100);
+            pedido.AdicionarItem(pedidoItem);
+
+            // Act & Assert
+            Assert.Throws<DomainException>(() => pedido.AdicionarItem(pedidoItem2));
+        }
     }
 }
