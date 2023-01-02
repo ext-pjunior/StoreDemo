6c597077ce723080ef98be67844e2e9ce74c690e
Paulo Junior <ext.paulo.junior@zamp.com.br>
2023-01-02 09:59:15 -0300

    adicionando testes

--- src/StoreDemo.Sales.Tests/PedidoTests.cs
+++ src/StoreDemo.Sales.Tests/PedidoTests.cs
@@ -19,5 +19,25 @@ namespace StoreDemo.Sales.Tests
             // Assert
             Assert.Equal(200, pedido.ValorTotal);
         }
+
+        [Fact(DisplayName = "Adicionar Item Pedido Existente")]
+        [Trait("Categoria", "Vendas - Pedido")]
+        public void AdicionarItemPedido_ItemExistente_DeveIncrementarUnidadesSomarValores()
+        {
+            // Arrange
+            var pedido = Pedido.PedidoFactory.NovoPedidoRascunho(Guid.NewGuid());
+            var produtoId = Guid.NewGuid();
+            var pedidoItem = new PedidoItem(produtoId, "Produto Teste", 2, 100);
+            pedido.AdicionarItem(pedidoItem);
+
+            // Act
+            var pedidoItem2 = new PedidoItem(produtoId, "Produto Teste", 1, 100);
+            pedido.AdicionarItem(pedidoItem2);
+
+            // Assert
+            Assert.Equal(300, pedido.ValorTotal);
+            Assert.Equal(1, pedido.PedidoItens.Count);
+            Assert.Equal(3, pedido.PedidoItens.FirstOrDefault(p => p.ProdutoId == produtoId).Quantidade);
+        }
     }
 }
