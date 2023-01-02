8f36c04f57fb2971e1bdee5e72b66d9e7e3d49ff
Paulo Junior <ext.paulo.junior@zamp.com.br>
2023-01-02 09:54:24 -0300

    adicionando testes e atualizando yaml

--- src/StoreDemo.Sales.Tests/PedidoItemTests.cs
+++ src/StoreDemo.Sales.Tests/PedidoItemTests.cs
@@ -5,12 +5,12 @@ namespace StoreDemo.Sales.Tests
 {
     public class PedidoItemTests
     {
-        //[Fact(DisplayName = "Novo Item Pedido com Unidades Abaixo do Permitido")]
-        //[Trait("Categoria", "Vendas - Pedido Item")]
-        //public void AdicionarItemPedido_UnidadesItemAbaixoDoPermitido_DeveRetornarException()
-        //{
-        //    // Arrange &  Act & Assert
-        //    Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "Produto Teste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
-        //}
+        [Fact(DisplayName = "Novo Item Pedido com Unidades Abaixo do Permitido")]
+        [Trait("Categoria", "Vendas - Pedido Item")]
+        public void AdicionarItemPedido_UnidadesItemAbaixoDoPermitido_DeveRetornarException()
+        {
+            // Arrange &  Act & Assert
+            Assert.Throws<DomainException>(() => new PedidoItem(Guid.NewGuid(), "Produto Teste", Pedido.MIN_UNIDADES_ITEM - 1, 100));
+        }
     }
 }
